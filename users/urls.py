87cebd155008fc6ba2e3e3ca08f262d40198f76b
Ryu Do Hee <rnrnzhs099@gmail.com>
2026-01-27 17:44:32 +0900

    Version #01 : 프로젝트 merge step3

--- users/urls.py
+++ users/urls.py
@@ -5,8 +5,8 @@ app_name = 'users'
 
 urlpatterns = [
     re_path(r"^signup/$", views.signup_view, name="signup"),
-    re_path(r'^mypage/$', views.mypage, name='mypage'),  # /users/mypage/
-    re_path(r'^mypage/edit/$', views.mypage_edit, name='mypage_edit'),  # /users/mypage/edit/
-    re_path(r'^mypage/activity/$', views.mypage_activity, name='mypage_activity'),  # /users/mypage/activity/
-    re_path(r'^profile/(?P<user_id>\d+)/$', views.user_profile, name='user_profile'), # /users/profile/사용자id숫자
+    re_path(r'^mypage/$', views.mypage, name='mypage'),                             # /users/mypage/
+    re_path(r'^mypage/edit/$', views.mypage_edit, name='mypage_edit'),                  # /users/mypage/edit/
+    re_path(r'^mypage/activity/$', views.mypage_activity, name='mypage_activity'),         # /users/mypage/activity/
+    re_path(r'^profile/(?P<user_id>\d+)/$', views.user_profile, name='user_profile'),         # /users/profile/사용자id숫자
 ]
